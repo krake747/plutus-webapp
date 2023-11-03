7cc37f3abd60d539adcff802d839dd8db5debe30
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-11-03 22:05:29 +0100

    chore(app): Remove greeter

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,5 @@
 import "./styles/main.scss";
-import "htmx.org";
 
-import { createGreetingContainer } from "./greeter/greeter";
 import { createBikesContainer } from "./bikes/bikes";
 
-document.body.appendChild(createGreetingContainer());
 document.body.appendChild(createBikesContainer());
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 module.exports = {
     entry: {
-        main: [path.resolve(__dirname, "./src/index.ts"), path.resolve(__dirname, "./src/greeter/greeter.ts")]
+        main: [path.resolve(__dirname, "./src/index.ts")]
     },
     output: {
         path: path.resolve(__dirname, "dist"),
